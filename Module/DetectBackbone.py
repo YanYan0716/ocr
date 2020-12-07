fdc89779d9b0ca468866deed6d63282317a44076
yanqian <yanqian0716@gmail.com>
2020-12-07 17:56:08 +0800

    .

--- Module/DetectBackbone.py
+++ Module/DetectBackbone.py
@@ -1,12 +1,29 @@
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.python.keras.models import Model
+from tensorflow.keras import layers, regularizers
+import numpy as np
+
+
 from Module.efficientnet.lite.efficientnet_lite_builder import build_model_base
+
+
+def unpool(inputs):
+    return tf.image.resize(inputs, size=[tf.shape(inputs)[1]*2, tf.shape(inputs)[2]*2])
 
 
 def normalize(image):
     return tf.cast(image, tf.float32) / 255.0
 
 
-class Backbone(object):
+def my_conv(inputs, output_num, keernel_size=1, activation_fc='relu', bn=True):
+    x = layers.Conv2D(output_num, keernel_size, activation=activation_fc, kernel_regularizer=regularizers.l2(1e-5))(inputs)
+    if bn==True:
+        x = layers.BatchNormalization(epsilon=1e-5, scale=True)(x)
+    return x
+
+
+class DetectModel(object):
     def __init__(self, is_training=True):
         self.is_training = is_training
 
@@ -19,5 +36,63 @@ class Backbone(object):
         '''
         # 图片归一化
         img = normalize(imgs)
+
         fts, endpoints = build_model_base(img, 'efficientnet-lite0', self.is_training)
         return fts, endpoints
+
+    def detect_eval_model(self, weights_dir, model_name, imgs=None):
+        assert model_name == 'EfficientNetB0', 'detect model is wrong'
+        base_model = keras.applications.EfficientNetB0(
+            include_top=False,
+            weights=weights_dir,
+            input_tensor=None,
+            input_shape=None,
+            pooling='avg',
+            classes=1000,
+            classifier_activation=None
+        )
+        model = Model(
+            inputs=base_model.input,
+            outputs=[
+                base_model.get_layer('normalization').output,
+                base_model.get_layer('block1a_project_bn').output,
+                base_model.get_layer('block2b_add').output,
+                base_model.get_layer('block3b_add').output,
+                base_model.get_layer('block5c_add').output,
+                base_model.get_layer('block7a_project_bn').output,
+                base_model.get_layer('avg_pool').output,
+            ]
+        )
+
+        with tf.GradientTape() as tape:
+            # 图片归一化
+            imgs = normalize(imgs)
+            output = model(imgs)
+        fts, endpoints = output[1:6], [output[0], output[-2]]
+
+        g = [None, None, None, None]
+        h = [None, None, None, None]
+
+        g_recong = [None, None, None, None, None]
+        h_recong = [None, None, None, None, None]
+        num_outputs_recong = [256, 128, 64, 32, 32]
+
+        for i in range(4):
+            if i == 0:
+                h[i] = my_conv(fts[i], num_outputs_recong[i], 1)
+            else:
+                f_t = my_conv(fts[i], num_outputs_recong[i], 1)
+                c1_1 = my_conv(tf.concat([g[i-1], f_t], axis=-1), num_outputs_recong[i], 1)
+                h[i] = my_conv(c1_1, num_outputs_recong[i], 3)
+
+            if i<=2:
+                g_recong[i] = unpool(h_recong[i])
+            else:
+                g_recong[i] = my_conv(h_recong[i], num_outputs_recong[i], 3)
+
+        F_score = my_conv(g[3],1,1, activation_fc='sigmoid', bn=False)
+        TEXT_SCALE = 512
+        geo_map = my_conv(g[3], 4, 1, activation_fc='sigmoid', bn=False)*TEXT_SCALE
+        angle_map = (my_conv(g[3], 1, 1, activation_fn='sigmoid', bn=False) - 0.5) * np.pi / 2
+        F_geometry = tf.concat([geo_map, angle_map], axis=-1)
+        return g_recong[4], F_score, F_geometry
--- test_img.py
+++ test_img.py
@@ -3,9 +3,11 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import cv2
 import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.models import Model
 
 from DataPreprocess.imgUtil import ResizeImg
-from Module.DetectBackbone import Backbone
+from Module.DetectBackbone import DetectModel
 
 if __name__ == '__main__':
     # 获取一张图片
@@ -19,17 +21,20 @@ if __name__ == '__main__':
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = tf.convert_to_tensor(img, dtype=tf.uint8)
     img = tf.expand_dims(img, axis=0)
-    print(img.shape)
+    print(f'img shape: {img.shape}')
 
     # 网络预测过程
-    model_path = './model_weights/efficientnet-lite0/model.ckpt'
-    detect_part = Backbone(is_training=False)
-    fts, endpoints = detect_part.model(img)
-    print(fts.shape)
-    print(endpoints['reduction_1'].shape)
-    print(endpoints['reduction_2'].shape)
-    print(endpoints['reduction_3'].shape)
-    print(endpoints['reduction_4'].shape)
-    print(endpoints['reduction_5'].shape)
+    weight_dir = './model_weights/efficientnetb0/efficientnetb0_notop.h5'
+    detectmodel = DetectModel()
+    detect_eval_model = detectmodel.detect_eval_model(weights_dir=weight_dir,
+                                                      model_name='EfficientNetB0',
+                                                      imgs=img)
+    # print(detect_eval_model.summary())
+    with tf.GradientTape() as tape:
+        output = detect_eval_model(img)
 
-    # step 1: detection
+    fts, endpoints = output[1:6], [output[0], output[-2]]
+    for i in range(len(fts)):
+        print(fts[i].shape)
+    for i in range(len(endpoints)):
+        print(endpoints[i].shape)
