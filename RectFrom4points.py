d47a828242e3cc46a46e0f5e407fed0daaa09a23
yanqian <yanqian0716@gmail.com>
2020-12-16 18:07:34 +0800

    .

--- RectFrom4points.py
+++ RectFrom4points.py
@@ -2,21 +2,18 @@ import itertools
 import tensorflow as tf
 
 
-def gen(m):
+def gen():
     m=8
     for i in itertools.count(1):
-        yield m, [1] * m
+        yield i
 
 
 if __name__ == '__main__':
-    a=gen(5)
+    # a=gen(5)
     dataset = tf.data.Dataset.from_generator(
-        a,
-        (tf.int64, tf.int64),
-        # (tf.TensorShape([]), tf.TensorShape([None]))
+        gen,
+        (tf.int64),
     )
 
-    # list(dataset.take(3).as_numpy_iterator())
-    for j in (dataset.take(3)):
+    for j in (dataset.take(6)):
         print(j)
-    # print(list(dataset.take(3).as_numpy_iterator()))
