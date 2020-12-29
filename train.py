3e2784cb9e087bb86e39da8a19ec11e28fc82b17
yanqian <yanqian0716@gmail.com>
2020-12-29 15:36:20 +0800

    .

--- train.py
+++ train.py
@@ -1,24 +1,25 @@
 import os
 
-from Module.RRotateLayer import RotateModel
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 import tensorflow.keras as keras
 
-from Module.DetectLoss import detect_loss, testloss
+from Module.DetectLoss import detect_loss
 from Module.RecognitionBackbone import Recognition_model
 from Module.RecognitionLoss import recognition_loss
 from Module.DetectBackbone import Detect_model
 from DataPreprocess.DataGen import generator
 
 if __name__ == '__main__':
-    MAX_EPOCHS = 2
+    MAX_EPOCHS = 1
     THETA = 0.9  # 控制检测和识别loss占总体loss的权重
     TRAIN = True
     CONTINUE_TRAIN = False
-    DETECT_WEIGHTS_DIR = './model_weights/detect_weights'
-    REG_WEIGHTS_DIR = './model_weights/reg_weights'
+    MODEL_WEIGHTS_DIR = './model_weights/summary_weights'
+    SAVE_MODEL = False
+    BEST_LOSS = 1000
+    LOSS_STEP = 20  # 设置评估loss的步长
+
     # 构建数据库
     # ----通过tf.data.Dataset.from_generator产生输入数据
     dataset = tf.data.Dataset.from_generator(
@@ -33,9 +34,6 @@ if __name__ == '__main__':
     weight_dir = './model_weights/efficientnetb0/efficientnetb0_notop.h5'
     detectmodel = Detect_model(base_weights_dir=weight_dir).model()
 
-    # 加入roi_rotate
-    rotatemodel = RotateModel().model()
-
     # 搭建Recognition网络
     regmodel = Recognition_model(lstm_hidden_num=256).model()
 
@@ -48,31 +46,38 @@ if __name__ == '__main__':
     inputs2 = detectmodel(inputs1)[0]
     inputs3 = regmodel.inputs[1]
     inputs4 = regmodel.inputs[2]
-
     all_inputs = [inputs1, inputs3, inputs4]
 
     outputs0 = detectmodel(inputs1)[0]
     outputs1 = detectmodel(inputs1)[1]
     outputs2 = detectmodel(inputs1)[2]
-
     all_outputs = [outputs0, outputs1, outputs2, regmodel([outputs0, inputs3, inputs4])]
+
     summary_model = keras.Model(all_inputs, all_outputs)
 
-    for layer in detectmodel.layers:
-        layer.trainable = False
-    detectmodel.layers[-1].trainable = True
+    if TRAIN:
+        for layer in detectmodel.layers:
+            layer.trainable = False
+        detectmodel.layers[-1].trainable = True
+        regmodel.trainable = True
+    else:
+        detectmodel.trainable = False
+        regmodel.trainable = False
+
+    if CONTINUE_TRAIN:
+        summary_model = keras.models.load_model(MODEL_WEIGHTS_DIR)
 
     print(len(summary_model.trainable_weights))
 
     optim = keras.optimizers.Adam()
 
     # 训练过程
-    for i in range(1):
+    for i in range(MAX_EPOCHS):
+        temp_loss = 0
         for batch, (
                 images, image_fns, score_maps, geo_maps, training_masks, transform_matrixes, boxes_masks, box_widths, \
                 text_labels_sparse_0, text_labels_sparse_1, text_labels_sparse_2) in enumerate(dataset):
             with tf.GradientTape() as tape:
-                print(images.shape)
                 input_box_masks = tf.expand_dims(boxes_masks, axis=0)
                 input_box_widths = tf.expand_dims(box_widths, axis=0)
                 input_box_info = tf.transpose(tf.concat([input_box_masks, input_box_widths], axis=0))
@@ -92,15 +97,25 @@ if __name__ == '__main__':
                                                    text_labels_sparse_1,
                                                    text_labels_sparse_2, )
 
-                total_loss = tf.cast(DetectLoss, dtype=tf.float32) + THETA * tf.cast(RecognitionLoss, dtype=tf.float32)
+            total_loss = tf.cast(DetectLoss, dtype=tf.float32) + THETA * tf.cast(RecognitionLoss, dtype=tf.float32)
             grad = tape.gradient([DetectLoss, RecognitionLoss], summary_model.trainable_weights)
-            optim.apply_gradients(zip(grad, summary_model.trainable_weights))
-            print(total_loss)
             # # 观察是否可以进行反向传播
             # j = 0
             # for i in range(len(grad)):
             #     if hasattr(grad[i], 'shape'):
             #         j += 1
             #         print(i, j, grad[i].shape)
+            optim.apply_gradients(zip(grad, summary_model.trainable_weights))
+
+            # 计算平均loss并保存模型
+            temp_loss += total_loss.numpy()
+            if (batch + 1) % LOSS_STEP == 0:
+                now_loss = temp_loss / 20.0
+                print(f'the loss is :{now_loss}')
+                if now_loss < BEST_LOSS:
+                    print(f'saving model to ----> {MODEL_WEIGHTS_DIR}')
+                    summary_model.save(MODEL_WEIGHTS_DIR)
+                temp_loss = 0
+
             break
         break
