2bbef1a3a1960a2f864984450b5974ceb55b19dd
yanqian <yanqian0716@gmail.com>
2021-01-05 17:58:21 +0800

    .

--- test_func.py
+++ test_func.py
@@ -2,15 +2,13 @@ from DataPreprocess.DataGen import load_annoatation
 import numpy as np
 
 
-if __name__ == '__main__':
-    gt_list = open('./icdar/train/GT.txt', 'r').readlines()
-    gt_list = [gt_mem.strip() for gt_mem in gt_list]
-    gt_list = np.array(gt_list)
+def func(a, b):
+    c = a+b
 
-    for i in range(len(gt_list)):
-        text_polys, text_tags, text_label = load_annoatation(gt_list[i])
-        # print(text_label)
-    # label = label.replace(' ', '')
-    # label = label.replace('')
-    # return [config.CHAR_VECTOR.index(x) for x in label]
-    print(i)
+    c+=1
+    return c
+
+
+if __name__ == '__main__':
+    a=func(1, 2)
+    print(a)
